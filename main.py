c5f38ea8253e907dc85abfce71749b73daad4837
Joel Stoker <joel.d.stoker@gmail.com>
2018-05-15 11:15:59 -0600

    finished utility

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def agent_pp2(quality):
     remaining = (1 - quality) /2
     return (quality, remaining, remaining)
 
-def agent_pp3(quality):
+def agent_pp3(quality, min_utility):
     remaining = (1 - quality) /2
-    return (quality, remaining, remaining, 0) # the foruth is else
+    return (quality, remaining, remaining, min_utility) # the foruth is else
 
 def problem1():
     quality = 0.1
@@ -76,19 +76,23 @@ def Bayes(priors, observation, likelihood):
         numerators.append(numerator)
         return list(map(lambda x : x / normalizer, numerators))
 
-def Bayes3(priors, observation, likelihood, utility_min):
+def Bayes3(priors, observation, likelihood, minimum_utility):
     normalizer = 0.0
+    real_likelihood = 0.0
     numerators = []
-    for i in range(len(priors)):
-        real_likelihood = 0.0
+    if observation == -1:
+        return priors
+    for i in range(3):
         if observation == i:
-            real_likelihood = likelhood
+            real_likelihood = likelihood
         else:
             real_likelihood = (1 - likelihood)/2
         numerator = priors[i] * real_likelihood
-        normalizer += numerator
+        normalizer = numerator + normalizer
         numerators.append(numerator)
-        return list(map(lambda x : x / normalizer, numerators))
+    partial = list(map(lambda x : x / normalizer, numerators))
+    partial.append(priors[3])
+    return partial
 
 def problem3():
     state1 = True
@@ -102,14 +106,23 @@ def problem3():
     agents = []
     first_agent_info = math.floor(NUM_AGENTS * percent_good)
     for i in range(first_agent_info):
-        agents.append(agent_pp3(quality))
+        agents.append(agent_pp3(quality, minimum_utility))
     for i in range(first_agent_info, NUM_AGENTS):
-        agents.append(agent_pp3(random.uniform(0.0, 1)))
+        agents.append(agent_pp3(random.uniform(0.0, 1), minimum_utility))
     # block
     decisions = []
     for i in range(NUM_AGENTS):
         for decision in decisions:
-            agents[i] = Bayes(agents[i], decision, likelihood)
-        decisions.append(agents[i].index(max(agents[i])) + 1)
+            agents[i] = Bayes3(agents[i], decision, likelihood, minimum_utility)
+        #decisions.append(agents[i].index(max(agents[i])) + 1)
+        #print(len(agents[i]))
+        #print(agents)
+        best = max(agents[i][:3])
+        if best < minimum_utility:
+            decisions.append(-1)
+        else:
+            decisions.append(agents[i].index(max(agents[i])) + 1)
     return decisions
             
+a = problem3()
+print(a)
